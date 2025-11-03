eed2d4468477c1c3ec0234aed61e003fd6caecc0
Xi Wu <youngjoycewu@gmail.com>
2025-11-04 02:34:25 +0800

    Wave01: Setup Task Class

--- app/models/task.py
+++ app/models/task.py
@@ -1,5 +1,15 @@
+from sqlalchemy import DateTime
 from sqlalchemy.orm import Mapped, mapped_column
+from datetime import datetime
+from typing import Optional
 from ..db import db
 
 class Task(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    title: Mapped[str]
+    description: Mapped[str]
+    completed_at: Mapped[Optional[datetime]] = mapped_column(
+        DateTime, 
+        nullable=True, 
+        default=None
+    )
