808a1cb1ed4ad04449d3ba1f1db4590a7e42e876
Xi Wu <youngjoycewu@gmail.com>
2025-11-05 22:19:10 +0800

    add on get_models_with_filters()

--- app/routes/route_utilities.py
+++ app/routes/route_utilities.py
@@ -31,3 +31,15 @@ def create_model(cls, model_data):
     db.session.commit()
 
     return new_model.to_dict(), 201
+
+def get_models_with_filters(cls, filters=None):
+    query = db.select(cls)
+    
+    if filters:
+        for attribute, value in filters.items():
+            if hasattr(cls, attribute):
+                query = query.where(getattr(cls, attribute).ilike(f"%{value}%"))
+
+    models = db.session.scalars(query.order_by(cls.id))
+    models_response = [model.to_dict() for model in models]
+    return models_response
