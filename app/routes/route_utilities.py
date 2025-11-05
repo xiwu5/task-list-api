817f3a336ce4a5d9b58374947f80a1ca1061b1af
Xi Wu <youngjoycewu@gmail.com>
2025-11-05 23:15:01 +0800

    edit response message of create_model()

--- app/routes/route_utilities.py
+++ app/routes/route_utilities.py
@@ -24,9 +24,10 @@ def create_model(cls, model_data):
         new_model = cls.from_dict(model_data)
         
     except KeyError as error:
-        response = {"message": f"Invalid request: missing {error.args[0]}"}
+        # response = {"message": f"Invalid request: missing {error.args[0]}"}
+        response = {"details": "Invalid data"}
         abort(make_response(response, 400))
-    
+        
     db.session.add(new_model)
     db.session.commit()
 
