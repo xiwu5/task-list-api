758f69917f164d9e80a6bfa32dca4ecdbe10f2af
Xi Wu <youngjoycewu@gmail.com>
2025-11-05 21:56:02 +0800

    Wave03:endpoint "/<task_id>/mark_incomplete" done!

--- app/routes/task_routes.py
+++ app/routes/task_routes.py
@@ -82,3 +82,11 @@ def mark_task_complete(task_id):
 
     return Response(status=204, mimetype="application/json")
 
+@tasks_bp.patch("/<task_id>/mark_incomplete")
+def mark_task_incomplete(task_id):
+    task = validate_model(Task, task_id)
+    task.completed_at = None
+    db.session.commit()
+
+    return Response(status=204, mimetype="application/json")
+
