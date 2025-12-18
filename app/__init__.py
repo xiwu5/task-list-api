1b5ad718c720773f29eebd2b2818c633dae73eeb
Xi <youngjoycewu@gmail.com>
2025-12-19 07:03:33 +0800

    install CORS

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,14 @@
 from flask import Flask
 from .db import db, migrate
 from .models import task, goal
+from flask_cors import CORS
 from .routes.task_routes import tasks_bp
 from .routes.goal_routes import goals_bp
 import os
 
 def create_app(config=None):
     app = Flask(__name__)
+    CORS(app)
 
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
